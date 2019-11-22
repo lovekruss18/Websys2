eb18c4b8966cdfcb416ccd6aacd94750238d8cab
lovekruss18 <48708698+lovekruss18@users.noreply.github.com>
2019-11-21 17:15:52 -0800

    Add files via upload

---
+++ post.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form  action = "displaymethod" method = "post">
+   Name: <input type="text" name="name"/>
+   Age :  <input type="text" name="age"/>
+   Password: <input type="password" name="pass"/>
+   <input type="submit" value="Submit">
+   </form>
+   <br><a href="index.php">back</a>
+</body>
+</html>
